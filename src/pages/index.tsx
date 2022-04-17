677dd90b05605edfdfd466789cc56d9407484a32
sojeda@funcionasi.es <sojeda@funcionasi.es>
2022-04-17 15:39:25 +0200

    create page and first component

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+import MainTitle from '../components/Maintitle.component';
+
+const Home: NextPage = () => {
+  
+  return (
+    <div className='text-regal-blue'>
+     <MainTitle></MainTitle>
+    </div>
+  );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'regal-blue': '#535373',
+      }
+    },
+    fontFamily: {
+      "indie":[ 'Indie Flower']
+    }
+  },
+  plugins: [],
+}
